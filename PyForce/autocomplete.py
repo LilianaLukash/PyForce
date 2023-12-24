835554d71cdfc5fbcac69248352776541c8b2370
bpavlyk <bogdan.pavlyk7@gmail.com>
2023-12-24 02:00:47 +0200

    Added autocomplete logic for all possible commands
    Added autocomplete logic for all possible commands

---
+++ PyForce/autocomplete.py
@@ -0,0 +1,34 @@
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+
+# You need to install the external package in the terminal first: pip install prompt_toolkit
+commands = WordCompleter(
+    [
+        "close",
+        "exit",
+        "end",
+        "bye",
+        "hello",
+        "hi",
+        "change",
+        "phone",
+        "all",
+        "delete",
+        "add-birthday",
+        "show-birthday",
+        "add-contact",
+        "birthdays",
+        "add-address",
+        "add-email",
+        "add-phone",
+        "findall",
+        "noteadd",
+        "notesall",
+        "notesedit",
+        "notesremove",
+        "findbytag",
+        "addtag",
+    ],
+    ignore_case=True,
+)
+session = PromptSession(completer=commands)
--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PyForce',
-    version='1',
-    description='Personal assistant for reminding about contacts\' birthdays',
-    url='https://github.com/LilianaLukash/PyForce',
-    author='PyForce',
-    author_email=['lukashliliana@gmail.com', 'artur.myhajlyuk@gmail.com', 'bogdan.pavlyk7@gmail.com', 'kucherkovpromotion@gmail.com'],
-    license='MIT',
+    name="PyForce",
+    version="1",
+    description="Personal assistant for reminding about contacts' birthdays",
+    url="https://github.com/LilianaLukash/PyForce",
+    author="PyForce",
+    author_email=[
+        "lukashliliana@gmail.com",
+        "artur.myhajlyuk@gmail.com",
+        "bogdan.pavlyk7@gmail.com",
+        "kucherkovpromotion@gmail.com",
+    ],
+    license="MIT",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'pyforce = pyforce.main:main_function',
-            'pyforce-models = pyforce.models:function_in_models',
-            'pyforce-notes = pyforce.modelsfornotes:function_in_modelsfornotes'
+        "console_scripts": [
+            "pyforce = pyforce.main:main_function",
+            "pyforce-models = pyforce.models:function_in_models",
+            "pyforce-notes = pyforce.modelsfornotes:function_in_modelsfornotes",
         ]
-    }
+    },
+    install_requires=[
+        "prompt_toolkit >= 3.0.43",
+    ],
 )
